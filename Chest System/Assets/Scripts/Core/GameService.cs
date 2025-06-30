f2629cc3ba0bd3070893a99534ed7630da262327
Imran1720 <skimran1720@gmail.com>
2025-06-30 16:23:38 +0530

    Added Services in GamesService and methods to retreive them.

--- Chest System/Assets/Scripts/Core/GameService.cs
+++ Chest System/Assets/Scripts/Core/GameService.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using ChestSystem.Chest;
+using ChestSystem.Player;
+using ChestSystem.UI;
+using ChestSystem.UI.PopUp;
+using ChestSystem.UI.Slot;
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class GameService : MonoBehaviour
+namespace ChestSystem.Core
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameService : MonoBehaviour
     {
+        [SerializeField] private UIService UIService;
+        private PlayerService playerService;
+        private ChestService chestService;
 
-    }
+        private CommandInvoker commandInvoker;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [Header("CHEST-DATA")]
+        [SerializeField] ChestSO chestSO;
+        [SerializeField] ChestView chestPrefab;
+        private void Start()
+        {
+            InitializeSevices();
+            commandInvoker = new CommandInvoker();
+            GetPopUpService().SetCommandInvoker(commandInvoker);
+        }
 
+        private void InitializeSevices()
+        {
+
+            playerService = new PlayerService(3, 3);
+            chestService = new ChestService(chestSO, chestPrefab);
+        }
+
+
+        private void Update()
+        {
+            chestService.Update();
+        }
+
+        public void ClearCommandHistory() => commandInvoker.ClearHistory();
+
+        public void ClearCommandHistory() => commandInvoker.ClearHistory();
+
+        public ChestService GetChestService() => chestService;
+        public PopUpService GetPopUpService() => UIService.GetPopUpService();
+        public PlayerService GetPlayerService() => playerService;
+        public SlotService GetSlotService() => UIService.GetSlotService();
+        public UIService GetUIService() => UIService;
     }
 }
--- Chest System/Assets/Scripts/UI/UIService.cs
+++ Chest System/Assets/Scripts/UI/UIService.cs
@@ -77,23 +77,6 @@ namespace ChestSystem.UI
 
         public void ClearCommandHistory() => commandInvoker.ClearHistory();
 
-        private void Update()
-        {
-            //if (Input.GetKeyUp(KeyCode.A))
-            //{
-            //    int timerValueInMinutes = 11;
-            //    float time = (float)timerValueInMinutes / 10;
-            //    int buyingCost = (int)Mathf.Ceil(time) * 10;
-            //    Debug.Log(time + " " + buyingCost);
-            //}
-
-            //if (Input.GetKeyUp(KeyCode.S))
-            //{
-            //    popUpService.ShowWarningPopUP();
-            //}
-
-            chestService.Update();
-        }
         private void UpdateCoinCount() => coinCountText.text = playerService.GetCoinCount().ToString();
         private void UpdateGemCount() => gemCountText.text = playerService.GetGemCount().ToString();
 
