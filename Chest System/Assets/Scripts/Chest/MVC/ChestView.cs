c3e9ee8d4cac6fc03d805234ab45c29b5098525b
Imran1720 <skimran1720@gmail.com>
2025-07-03 16:54:09 +0530

    Refactored ChestView

--- Chest System/Assets/Scripts/Chest/MVC/ChestView.cs
+++ Chest System/Assets/Scripts/Chest/MVC/ChestView.cs
@@ -1,69 +1,77 @@
 using ChestSystem.Chest;
 using ChestSystem.Events;
-using ChestSystem.UI;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ChestView : MonoBehaviour, IPointerDownHandler
+namespace ChestSystem.Chest
 {
-    [SerializeField] private TextMeshProUGUI chestStateText;
-    [SerializeField] private TextMeshProUGUI OpenDurationText;
-    [SerializeField] private TextMeshProUGUI openingCostText;
-
-    [SerializeField] private Image chestIcon;
-
-    [SerializeField] private GameObject lockedStateUI;
-    [SerializeField] private GameObject payUI;
-    [SerializeField] private GameObject OpenUI;
-
-    [SerializeField] private Image background;
-    [SerializeField] private Color openBGColor;
-    private Color defaultBGColor;
-
-    private EventService eventService;
-
-    private void Start() => defaultBGColor = background.color;
-    public void SetServices(EventService eventService) => this.eventService = eventService;
-    public void OnPointerDown(PointerEventData eventData) => eventService.OnChestSelected.InvokeEvent(this);
-    public void SetState(EChestState state) => chestStateText.text = GetChestStateText(state);
-    public void SetTimer(int hour, int minute)
+    public class ChestView : MonoBehaviour, IPointerDownHandler
     {
-        OpenDurationText.text = "";
-        OpenDurationText.text += minute + "M";
-    }
+        [Header("UI Text")]
+        [SerializeField] private TextMeshProUGUI chestStateText;
+        [SerializeField] private TextMeshProUGUI openDurationText;
+        [SerializeField] private TextMeshProUGUI openingCostText;
 
-    public void SetChestIcon(Sprite icon) => chestIcon.sprite = icon;
-    public void SetOpeningCost(int cost) => openingCostText.text = cost.ToString();
+        [Header("UI Image")]
+        [SerializeField] private Image chestIcon;
+        [SerializeField] private Image background;
 
-    public string GetChestStateText(EChestState state)
-    {
-        switch (state)
+        [Header("Game Objects")]
+        [SerializeField] private GameObject lockedStateUI;
+        [SerializeField] private GameObject payUI;
+        [SerializeField] private GameObject openUI;
+
+        [Header("Color")]
+        [SerializeField] private Color openBGColor;
+        private Color defaultBGColor;
+
+        private EventService eventService;
+
+        private void Start()
         {
-            case EChestState.COLLECTED:
-                return "COLLECTED";
-            case EChestState.UNLOCKING:
-                return "UNLOCKING";
-            case EChestState.UNLOCKED:
-                return "UNLOCKED";
-            default:
-                return "LOCKED";
+            defaultBGColor = background.color;
         }
+
+        public void SetTimer(int hour, int minute)
+        {
+            //hour - future addition
+            openDurationText.text = $"{minute}M";
+        }
+
+        public string GetChestStateText(EChestState state)
+        {
+            switch (state)
+            {
+                case EChestState.COLLECTED:
+                    return "COLLECTED";
+                case EChestState.UNLOCKING:
+                    return "UNLOCKING";
+                case EChestState.UNLOCKED:
+                    return "UNLOCKED";
+                default:
+                    return "LOCKED";
+            }
+        }
+
+        public void SetLockedUI(bool isLocked, EChestState chestState)
+        {
+            SetState(chestState);
+            lockedStateUI.SetActive(isLocked);
+            payUI.SetActive(isLocked);
+            openUI.SetActive(!isLocked);
+        }
+
+        public void OnPointerDown(PointerEventData eventData) => eventService?.OnChestSelected.InvokeEvent(this);
+
+        public void SetServices(EventService eventService) => this.eventService = eventService;
+        public void SetState(EChestState state) => chestStateText.text = GetChestStateText(state);
+
+
+        public void Reset() => background.color = defaultBGColor;
+        public void SetChestIcon(Sprite icon) => chestIcon.sprite = icon;
+        public void SetOpenedChestBG() => background.color = openBGColor;
+        public void SetOpeningCost(int cost) => openingCostText.text = cost.ToString();
     }
-
-    public void SetLockedUI(bool value, EChestState chestState)
-    {
-        SetState(chestState);
-        lockedStateUI.SetActive(value);
-        payUI.SetActive(value);
-        OpenUI.SetActive(!value);
-    }
-
-    public void Reset() => background.color = defaultBGColor;
-    public void SetOpenedChestBG() => background.color = openBGColor;
-
 }
